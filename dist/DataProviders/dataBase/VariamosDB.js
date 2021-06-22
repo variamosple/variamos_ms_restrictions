e46b1848a17a77c17bb40d4353f25c23a6e376af
Mauro Agudelo <mauro.0116@gmail.com>
2021-06-21 22:40:49 -0500

    Add new database

--- dist/DataProviders/dataBase/VariamosDB.js
+++ dist/DataProviders/dataBase/VariamosDB.js
@@ -2,10 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.pool = void 0;
 const pg_1 = require("pg");
+// export const pool = new Pool ({
+//     user: 'adminpg@variamosdbsvr',
+//     host: 'variamosdbsvr.postgres.database.azure.com',
+//     password: 'D6w9yRIWw7r92opvkVzp',
+//     database: 'VariamosDB',
+//     port: 5432
+// })
 exports.pool = new pg_1.Pool({
-    user: 'adminpg@variamosdbsvr',
-    host: 'variamosdbsvr.postgres.database.azure.com',
-    password: 'D6w9yRIWw7r92opvkVzp',
-    database: 'VariamosDB',
-    port: 5432
+    user: "adminpg",
+    host: "variamos-db.postgres.database.azure.com",
+    password: "D6w9yRIWw7r92opvkVzp",
+    database: "VariamosDB",
+    port: 5432,
+    ssl: true,
 });
--- src/DataProviders/dataBase/VariamosDB.ts
+++ src/DataProviders/dataBase/VariamosDB.ts
@@ -1,11 +1,18 @@
-import { Pool } from 'pg'
+import { Pool } from "pg";
 
+// export const pool = new Pool ({
+//     user: 'adminpg@variamosdbsvr',
+//     host: 'variamosdbsvr.postgres.database.azure.com',
+//     password: 'D6w9yRIWw7r92opvkVzp',
+//     database: 'VariamosDB',
+//     port: 5432
+// })
 
-export const pool = new Pool ({
-    user: 'adminpg@variamosdbsvr',
-    host: 'variamosdbsvr.postgres.database.azure.com',
-    password: 'D6w9yRIWw7r92opvkVzp',
-    database: 'VariamosDB',
-    port: 5432
-})
-
+export const pool = new Pool({
+  user: "adminpg",
+  host: "variamos-db.postgres.database.azure.com",
+  password: "D6w9yRIWw7r92opvkVzp",
+  database: "VariamosDB",
+  port: 5432,
+  ssl: true,
+});
